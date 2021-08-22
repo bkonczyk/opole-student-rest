029ddde32bb411dd0739f6990b2d45e49cfff770
bkonczyk <bartkon@gmail.com>
2021-08-22 12:00:40 +0200

    initial commit

---
+++ src/test/java/pl/sda/spring/student/StudentApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.sda.spring.student;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
